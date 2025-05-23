36ee982b3049c3455e69516500981f1ab5eda906
homepai26 <homepai26@gmail.com>
2025-05-24 01:03:56 +0700

    fixed typo and move check out of loop

--- insfont.sh
+++ insfont.sh
@@ -2,6 +2,7 @@
 
 FONTSDIR="/usr/share/fonts"
 USER_LOCAL_FONTSDIR=~/.local/share/fonts
+IS_SELECT_FONT=0
 
 errexit() {
     echo -e "\033[0;31mError!\033[0m $1" 1>&2
@@ -18,7 +19,17 @@ find_fonts() {
     FONTSLIST="$FONTSLIST $(find $directory/*.otf 2> /dev/null)"
 }
 
-usuages() {
+create_directory() {
+    echo "Not found $install_folder/insfonts, Making folder."
+    mkdir -p $install_folder/insfonts
+    
+    if [ ! -d $install_folder/insfonts ]
+    then
+	errexit "Cannot new folder $install_folder/insfonts."
+    fi
+}
+
+usages() {
     cat <<EOF
 insfont is a script to install fonts from current directory, or target
 directory, or target file.
@@ -26,7 +37,7 @@ directory, or target file.
 Possible flags are:
  -d, --directory dir          Install multiple fonts from THAT directory.
  -f, --file *.[ttf,otf]       Install single font by target THAT file.
- -h, --help                   Print usuages.
+ -h, --help                   Print usages.
 EOF
 }
 
@@ -43,6 +54,7 @@ do
 	    if [ $2 ]
 	    then 
 		target_font $2
+		IS_SELECT_FONT=1
 		shift 2
 	    fi
 	    ;;
@@ -51,38 +63,39 @@ do
 	    if [ $2 ]
 	    then
 		find_fonts $2
+		IS_SELECT_FONT=1
 		shift 2
 	    fi
 	    ;;
 	
 	*)
-	    find_fonts $PWD
+	    # do not searching, if have been specified to-go *font* already.
+	    if [ $IS_SELECT_FONT == 0 ]
+	    then
+		find_fonts $PWD
+	    fi
+	    
+	    if [ $UID == 0 ]
+	    then
+		install_folder=$FONTSDIR
+	    else
+		install_folder=$USER_LOCAL_FONTSDIR
+	    fi
+	    
+	    if [ ! -d $install_folder/insfonts ]
+	    then
+		create_directory
+	    fi
+
 	    for i in $FONTSLIST
 	    do
-		if [ $UID == 0 ]
-		then
-		    install_folder=$FONTSDIR
-		else
-		    install_folder=$USER_LOCAL_FONTSDIR
-		fi
-		
-		echo Moving $i to $install_folder/insfonts.
-		if [ ! -d $install_folder/insfonts ]
-		then
-		    echo "Not found $install_folder/insfonts, Making folder."
-		    mkdir -p $install_folder/insfonts
-		    if [ ! -d $install_folder/insfonts ]
-		    then
-			errexit "Cannot new folder $install_folder/insfonts."
-		    fi
-		fi
-		
+		echo Moving $i to $install_folder/insfonts.		
 		mv $i $install_folder/insfonts
 	    done
 	    
 	    if [ ! $i ]
 	    then
-		usuages
+		usages
 		break
 	    fi
 	    
